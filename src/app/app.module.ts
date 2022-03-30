7777aba8658dcdd66836095acfd041f54943f0be
PjyothiK <jyothi.palli30@gmail.com>
2022-03-30 13:33:29 +0530

    5 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CounterComponent } from './counter/counter.component';
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterButtonsComponent } from './counter-buttons/counter-buttons.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,CounterComponent,CounterOutputComponent ,CounterButtonsComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
